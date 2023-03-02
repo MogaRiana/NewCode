ed38cc9d607a3c3414b102f80649864f2c0c3baa
MogaRiana <rianamoga08@gmail.com>
2023-03-02 21:48:01 +0200

    eep

--- Kattis/C++/A_Real_Challenge/main.cpp
+++ Kattis/C++/A_Real_Challenge/main.cpp
@@ -1,15 +1,14 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
-    long long a;
-    cin >> a;
-    double f = 4 * sqrt(a);
-    cout.precision(18);
-    cout << fixed;
-    cout << f;
-    return 0;
+int main() {
+  long long a;
+  cin >> a;
+  double f = 4 * sqrt(a);
+  cout.precision(18);
+  cout << fixed;
+  cout << f;
+  return 0;
 }

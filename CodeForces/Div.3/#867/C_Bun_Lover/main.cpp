13a67f834134f905158c0eb57f89e1b31f04db35
MogaRiana <rianamoga08@gmail.com>
2023-04-26 21:32:11 +0300

    eep

---
+++ CodeForces/Div.3/#867/C_Bun_Lover/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+typedef long long i64;
+typedef int i32;
+typedef short i16;
+typedef vector<i64> vi64;
+typedef vector<vi64> vv;
+typedef string str;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  i64 t;
+  cin >> t;
+
+  while (t--) {
+    i64 n;
+    cin >> n;
+
+    cout << 4 * n + n + 2 + (n * (n - 3)) << endl;
+  }
+
+  return 0;
+}

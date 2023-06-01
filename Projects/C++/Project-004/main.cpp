e2cc9e370702937b308a8293e2ac6cdbd3d29b65
MogaRiana <rianamoga08@gmail.com>
2023-06-01 08:43:47 +0300

    eep

---
+++ Projects/C++/Project-004/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class elevi {
+private:
+  int nota;
+  int absente;
+
+public:
+  void setnota(int nota) { this->nota = nota; }
+};
+
+class Catalog {
+private:
+  int n;
+  vector<elevi> eep;
+
+public:
+  void citire(vector<elevi> &eep) {
+    //.....
+  }
+};
+
+class Notebook : Catalog {
+private:
+  int nigger;
+
+public:
+  int white_boy;
+};
+
+int main() {
+  Catalog clasa_a;
+
+  // clasa_a.setnota();
+
+  Notebook eep;
+
+  vector<int> mata;
+}

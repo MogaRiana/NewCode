f0b1c1f861648ea153c898111bc85fd2d1ab7a9a
MogaRiana <rianamoga08@gmail.com>
2023-07-22 21:49:51 +0300

    eep

---
+++ Kattis/C++/Another_Candies/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+typedef long long i64;
+typedef int i32;
+typedef short i16;
+typedef vector<i64> vi64;
+typedef vector<vi64> vv;
+typedef string str;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  i64 t;
+  cin >> t;
+
+  while (t--) {
+    i64 n;
+    cin >> n;
+
+    i64 sum = 0;
+    for (i64 i = 0; i < n; i++) {
+      i64 x;
+      cin >> x;
+      sum += x % n;
+    }
+
+    if (sum % n == 0) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }
+
+  return 0;
+}

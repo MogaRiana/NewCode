3b7271ea51e8badfc42c6700a3e3df52d84b5169
MogaRiana <rianamoga08@gmail.com>
2023-09-21 23:34:47 +0300

    eep

--- Kattis/C++/Help_Me_With_The_Game/main.cpp
+++ Kattis/C++/Help_Me_With_The_Game/main.cpp
@@ -10,6 +10,44 @@ typedef vector<i64> vi64;
 typedef vector<vi64> vv;
 typedef string str;
 
+void print(map<pair<i64, char>, vector<pair<i64, char>>> &m) {
+  bool ok = true;
+  for (auto &x : m) {
+    for (i64 i = 0; i < x.second.size(); i++) {
+      if (ok) {
+        cout << ' ';
+        ok = false;
+      } else {
+        cout << ',';
+      }
+      if (x.first.second != 'P') {
+        cout << x.first.second;
+      }
+      cout << x.second[i].second << x.second[i].first;
+    }
+  }
+}
+
+map<pair<i64, char>, vector<pair<i64, char>>>
+make(map<pair<i64, char>, vector<pair<i64, char>>> &m, str &s, i64 j, i64 posc,
+     i64 posl) {
+  if (s[j] == 'k' or s[j] == 'K') {
+    m[{1, 'K'}].push_back({posc, posl});
+  } else if (s[j] == 'q' or s[j] == 'Q') {
+    m[{2, 'Q'}].push_back({posc, posl});
+  } else if (s[j] == 'r' or s[j] == 'R') {
+    m[{3, 'R'}].push_back({posc, posl});
+  } else if (s[j] == 'b' or s[j] == 'B') {
+    m[{4, 'B'}].push_back({posc, posl});
+  } else if (s[j] == 'n' or s[j] == 'N') {
+    m[{5, 'N'}].push_back({posc, posl});
+  } else {
+    m[{6, 'P'}].push_back({posc, posl});
+  }
+
+  return m;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
@@ -17,21 +55,32 @@ int main() {
 
   i64 posl = 8;
   str s;
-  map<char, vector<pair<char, i64>>> b;
-  map<char, vector<pair<char, i64>>> w;
+  map<pair<i64, char>, vector<pair<i64, char>>> b;
+  map<pair<i64, char>, vector<pair<i64, char>>> w;
   for (i64 i = 0; i < 8; i++, posl--) {
     char posc = 'a';
     cin >> s >> s;
 
     for (i64 j = 2; j < s.size(); j += 4, posc++) {
       if (s[j] >= 'a' and s[j] <= 'z') {
-        b[s[j]].push_back({posc, posl});
+        make(b, s, j, posl, posc);
       } else if (s[j] >= 'A' and s[j] <= 'Z') {
-        w[s[j]].push_back({posc, posl});
+        make(w, s, j, posl, posc);
       }
     }
   }
   cin >> s;
+
+  for (auto &x : w) {
+    sort(x.second.begin(), x.second.end());
+  }
+
+  cout << "White:";
+  print(w);
+  cout << endl;
+  cout << "Black:";
+  print(b);
+  cout << endl;
 
   return 0;
 }
